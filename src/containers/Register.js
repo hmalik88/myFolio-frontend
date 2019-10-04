28fb48218421c1a2192cc257830167abb4adda51
hmalik88 <hbmalik88@gmail.com>
2019-10-04 02:14:23 -0400

    basic register form in

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Form, FormGroup, FormText, Input, Label, Button } from 'reactstrap';
+import RegisterForm from '../components/RegisterForm';
 import '../scss/Register.scss';
 
 function Register() {
@@ -10,10 +11,21 @@ function Register() {
   }, [])
 
   return (
-    <Container fluid>
-      <Row>
-        <Col className='justify-content-center align-items-center'>
-
+    <Container className='register-form-container'>
+      <Row className='register-row'>
+        <Col className='justify-content-center'>
+          <Form>
+            <h2 className='register-title'>Sign up for an account!</h2>
+            <FormGroup>
+              <Label for='register-email'>E-mail</Label>
+              <Input type='email' id='register-email' />
+            </FormGroup>
+            <FormGroup>
+              <Label for='register-password'>Password</Label>
+              <Input size='md' id='register-password' type='password' />
+            </FormGroup>
+            <Button className='register-form-btn'>Register</Button>
+          </Form>
         </Col>
       </Row>
     </Container>
